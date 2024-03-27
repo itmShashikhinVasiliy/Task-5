947573cd63e1bfb37c1511a0c13500e4128616da
Belek <belek.salchak.97@mail.ru>
2024-03-27 19:55:02 +0700

    Task-5

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,33 +1,49 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
+      AnnotationConfigApplicationContext context =
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User belek = new User("Belek", "Salchak", "user1@mail.ru");
+      User ivan = new User("Ivan", "Ivanov", "user2@mail.ru");
+      User boris = new User("Boris", "Popov", "user3@mail.ru");
+      User vadim = new User("Vadim", "Nemkov", "user4@mail.ru");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car toyota = new Car("Toyota", 123);
+      Car honda = new Car("Honda", 456);
+      Car bmw = new Car("BMW", 789);
+      Car lada = new Car("Lada", 101);
+
+      userService.add(belek.setCar(toyota).setUser(belek));
+      userService.add(ivan.setCar(honda).setUser(ivan));
+      userService.add(boris.setCar(bmw).setUser(boris));
+      userService.add(vadim.setCar(lada).setUser(vadim));
+
+      for (User user : userService.listUsers()){
+         System.out.println(user+" "+ user.getCar());
       }
+
+      System.out.println(userService.getUserByCar("BMW", 789));
+
+      try {
+         User notUser = userService.getUserByCar("Subaru", 3453);
+      }catch (NoResultException e){
+         System.out.println("Такой пользователь отсутсвует");
+      }
+
 
       context.close();
    }
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -13,19 +13,24 @@ public class User {
    @Column(name = "name")
    private String firstName;
 
-   @Column(name = "last_name")
+   @Column(name = "lastname")
    private String lastName;
 
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   private Car car;
+
    public User() {}
-   
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
    }
+
+
 
    public Long getId() {
       return id;
@@ -57,5 +62,22 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar(){
+      return car;
+   }
+
+   public Car setCar(Car car){
+      this.car = car;
+      return car;
+   }
+
+   @Override
+   public String toString() {
+      return "firstName: " + firstName + ", " +
+              "lastName: " + lastName + ", " +
+              "email: " + email + ", " +
+              car.toString();
    }
 }
